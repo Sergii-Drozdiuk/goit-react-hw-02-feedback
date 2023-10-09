36c6e6b102981bd7759bf8152bf30fe2614fa528
Sergii-Drozdiuk <drozduyk@gmail.com>
2023-10-09 13:17:52 +0300

    add-icons

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
+import { FcFeedback } from 'react-icons/fc';
 
 export const Section = ({ title, children }) => {
   return (
     <section className="w-80 rounded-lg border p-2 max-[375px]:w-72">
-      <h2 className="mb-4 text-center text-2xl">{title}</h2>
+      <h2 className="flex items-center justify-center gap-2 mb-4 text-2xl"><FcFeedback size={40} />{title}</h2>
       {children}
     </section>
   );
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
+import { FcStatistics } from 'react-icons/fc';
 
 export const Statistics = ({ feedback: { good, neutral, bad }, totalFeedback, positiveFeedback, notificationText }) => {
 	const color = positiveFeedback >= 50 ? { color: 'rgb(163 230 53)' } : { color: 'rgb(248 113 113)' };
 	return (
 		<>
-			<h2 className="mb-2 text-center text-2xl">Statistics</h2>
+			<h2 className="mb-2 flex items-center justify-center gap-2 text-2xl"><FcStatistics size={32} />Statistics</h2>
 			{totalFeedback ? (
 				<ul className="mb-2 text-center">
 					<li className="text-lime-400">Good: {good}</li>
